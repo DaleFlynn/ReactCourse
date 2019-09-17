76e31b07fce12fa601c51c94736fa3f76a632845
DaleFlynn <55194144+DaleFlynn@users.noreply.github.com>
2019-09-17 15:44:17 +0100

    Addition of blogpost approval cards w/ faker and semantic ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 //Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 function getName(){
   return "Dale Parkes is gay";
@@ -18,7 +21,35 @@ const App = () => {
       <div>
         <h3>{getName()}</h3>
       </div>
+      //Blogpost cards
+      <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Jack"
+          timeAgo="Today at 16:45"
+          commentText="No way!"
+          profilePic={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Ste"
+          timeAgo="Today at 13:22"
+          commentText="Yes way!"
+          profilePic={faker.image.avatar()}
+         />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Dale"
+          timeAgo="Yesterday at 22:43"
+          commentText="Haha Ste!"
+          profilePic={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
+    </div>
+
   );
 }
 
