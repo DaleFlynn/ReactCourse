cad8158f5deb115b6b6de6de7a3063bd78316ba6
DaleFlynn <55194144+DaleFlynn@users.noreply.github.com>
2019-09-19 17:39:40 +0100

    Added geolocation request with lat output and error handling

--- src/index.js
+++ src/index.js
@@ -9,39 +9,60 @@ import ApprovalCard from './ApprovalCard';
 
 //Create a React component
 //Blogpost approval card
-const App = () => {
-  return (
-    <div>
+class App extends React.Component{
 
-      <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Jack"
-          timeAgo="Today at 16:45"
-          commentText="No way!"
-          profilePic={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Ste"
-          timeAgo="Today at 13:22"
-          commentText="Yes way!"
-          profilePic={faker.image.avatar()}
-         />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="Dale"
-          timeAgo="Yesterday at 22:43"
-          commentText="Haha Ste!"
-          profilePic={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
-    </div>
+  constructor(props){
+    super(props);
 
-  );
+    this.state = {lat: null, errorMessage: ''};
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude});
+      },
+      err => {
+        this.setState({errorMessage: err.message});
+      }
+    );
+  }
+
+  render (){
+    return (
+      <div>
+        <div>Latitude: {this.state.lat}</div>
+        <br />
+        <div>Error: {this.state.errorMessage}</div>
+
+        <div className="ui container comments">
+        <ApprovalCard>
+          <CommentDetail
+            author="Jack"
+            timeAgo="Today at 16:45"
+            commentText="No way!"
+            profilePic={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="Ste"
+            timeAgo="Today at 13:22"
+            commentText="Yes way!"
+            profilePic={faker.image.avatar()}
+           />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="Dale"
+            timeAgo="Yesterday at 22:43"
+            commentText="Haha Ste!"
+            profilePic={faker.image.avatar()}
+          />
+        </ApprovalCard>
+      </div>
+      </div>
+
+    );
+  };
 }
 
 //Take the React component and show it on screen
